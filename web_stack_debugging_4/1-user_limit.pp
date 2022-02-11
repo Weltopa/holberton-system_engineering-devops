2b4be442bf2554805efd8967058d24cfe9006331
Weltopa <patrick.welton@yahoo.com>
2022-02-11 07:24:33 -0800

    attempt

---
+++ web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# Increase limit
+exec { 'increase limit":
+  command => 'sed -i "/holberton hard/s/5/9999/" /etc/security/limits.conf',
+  path    => '/bin/'
+}
+
+# Increase alt limit
+exec { 'increase limit':
+  command => 'sed -i "/holberton soft/s/4/9999/" /etc/security/limits.conf',
+  path    => '/bin/'
+}
